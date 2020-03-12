541cc2755deb038718f095d27c291b4d8e1ab83f
TahirKMohammed <tmohammed@ryerson.ca>
2020-03-12 16:50:08 -0400

    Create MyEdge.java

---
+++ MyEdge.java
@@ -0,0 +1,16 @@
+/**
+ *
+ * @author kingtahir
+ */
+public class MyEdge {
+    
+    private double weight;
+    
+    public MyEdge(double w){
+        this.weight = w;
+    }
+    
+    public double getWeight(){
+        return this.weight;
+    }
+}
